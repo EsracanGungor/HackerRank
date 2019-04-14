75e6b22564e755e25a4889f5256121ed807b5ac6
EsracanGungor <47601189+EsracanGungor@users.noreply.github.com>
2019-04-14 20:26:06 +0300

    Create Pointer.cpp

---
+++ Pointer.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+        int sum=*a+*b;
+      int c;
+  if(*a<*b)
+       c=-(*a-*b);
+    else
+    c=*a-*b;
+    *a=sum;
+    *b=c;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
+
